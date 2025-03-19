d6442b0c1c549aabfc6cb5bc05b2cb202c72cd20
Fuonder <ranygi23@mail.ru>
2025-03-20 00:34:38 +0300

    flags 1st version

---
+++ cmd/gophermart/flags.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+POSTGRE USER && DATABASE:
+
+create user market with encrypted password '12345678';
+create database market;
+grant all privileges on database market to market;
+\c market;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO market;
+GRANT ALL ON SCHEMA public TO market;
+GRANT CREATE ON SCHEMA public to market;
+GRANT USAGE ON SCHEMA public to market;
+
+*/
+
+var (
+	version  = "0.0.1"
+	progName = "Fuonder's gopherstore"
+	source   = "https://github.com/Fuonder/goptherstore"
+)
+
+var usage = func() {
+	fmt.Fprintf(flag.CommandLine.Output(), "%s\nSource code:\t%s\nVersion:\t%s\nUsage of %s:\n",
+		progName,
+		source,
+		version,
+		progName)
+	flag.PrintDefaults()
+}
+
+var (
+	ErrNotFullIP   = errors.New("given ip address and port incorrect")
+	ErrInvalidIP   = errors.New("incorrect ip address")
+	ErrInvalidPort = errors.New("incorrect port number")
+)
+
+type netAddress struct {
+	ipaddr string
+	port   int
+}
+
+func (n *netAddress) String() string {
+	return fmt.Sprintf("%s:%d", n.ipaddr, n.port)
+}
+func (n *netAddress) Set(value string) error {
+	values := strings.Split(value, ":")
+	if len(values) != 2 {
+		return fmt.Errorf("%w: \"%s\"", ErrNotFullIP, value)
+	}
+	n.ipaddr = values[0]
+	if n.ipaddr == "" {
+		return fmt.Errorf("%w: \"%s\"", ErrInvalidIP, values[0])
+	}
+	var err error
+	n.port, err = strconv.Atoi(values[1])
+	if err != nil {
+		return fmt.Errorf("%w: \"%s\"", ErrInvalidPort, values[1])
+	}
+	return nil
+}
+
+type Flags struct {
+	APIAddress     netAddress
+	AccrualAddress netAddress
+	DatabaseDSN    string
+	LogLevel       string
+}
+
+func (f *Flags) String() string {
+	return fmt.Sprintf("APIAddress: %s, "+
+		"AccrualAddress: %s, "+
+		"DatabaseDSN: %s, "+
+		"LogLevel: %s",
+		f.APIAddress.String(),
+		f.AccrualAddress.String(),
+		f.DatabaseDSN,
+		f.LogLevel,
+	)
+}
+
+var (
+	CliOptions = Flags{
+		APIAddress: netAddress{
+			ipaddr: "localhost",
+			port:   1234,
+		},
+		AccrualAddress: netAddress{
+			ipaddr: "localhost",
+			port:   8080,
+		},
+		DatabaseDSN: "postgres://market:12345678@localhost:5432/market?sslmode=disable",
+		LogLevel:    "info",
+	}
+)
+
+func parseFlags() error {
+	flag.Usage = usage
+	flag.Var(&CliOptions.APIAddress, "a", "ip and port of server in format <ip>:<port>")
+	flag.Var(&CliOptions.AccrualAddress, "r", "ip and port of accrual service in format <ip>:<port>")
+	flag.StringVar(&CliOptions.DatabaseDSN, "d", "postgres://market:12345678@localhost:5432/market?sslmode=disable", "Database DSN")
+	flag.StringVar(&CliOptions.LogLevel, "l", "info", "loglevel")
+
+	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDRESS"); envRunAddr != "" {
+		err := CliOptions.APIAddress.Set(envRunAddr)
+		if err != nil {
+			return err
+		}
+	}
+	if envAccrualAddr := os.Getenv("ACCRUAL_SYSTEM_ADDRESS"); envAccrualAddr != "" {
+		err := CliOptions.AccrualAddress.Set(envAccrualAddr)
+		if err != nil {
+			return err
+		}
+	}
+	if envDatabaseDSN := os.Getenv("DATABASE_DSN"); envDatabaseDSN != "" {
+		CliOptions.DatabaseDSN = envDatabaseDSN
+	}
+
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		CliOptions.LogLevel = envLogLevel
+	}
+
+	return nil
+}
--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	err := parseFlags()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(CliOptions.String())
+
+	err = run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+
+	return nil
+}
